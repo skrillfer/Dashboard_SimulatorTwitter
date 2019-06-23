04c9def94eed63fb7da0c125f4a89997e96281a1
fernando <rluis4490@gmail.com>
2019-06-22 23:27:21 -0600

    server received data from python

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
@@ -13,6 +16,11 @@ app.get('/', function (req, res) {
   //res.sendFile(__dirname+'/public/src/login.html');
 });
 
+app.post('/data', function (req, res) {    
+    //do stuff with the data here
+    console.log(req.body)
+    res.send("process complete");
+});
 server.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
