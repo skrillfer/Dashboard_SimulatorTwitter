845f4c497b7fcbb9428363bf634cc9b03ac740ed
fernando <rluis4490@gmail.com>
2019-06-23 02:07:44 -0600

    link resources

--- index.js
+++ index.js
@@ -24,3 +24,9 @@ app.post('/data', function (req, res) {
 server.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
+
+/*
+https://www.quora.com/How-do-I-send-data-from-Node-js-to-Python-and-get-back-JSONN-data-from-Python-to-Node-jss
+https://stackoverflow.com/questions/23370718/python-socket-sendall-randomly-fails
+https://stackoverflow.com/questions/42415207/send-receive-data-with-python-socket
+*/
